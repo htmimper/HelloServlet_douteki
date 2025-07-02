7e95b872c7f52445f00e6f99ecad8923bceffc3c
htmimper <htmimper@gmail.com>
2025-07-02 21:37:34 +0900

    Add files via upload

--- HelloServlet/src/main/java/HelloServlet.java
+++ HelloServlet/src/main/java/HelloServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse; 
 @WebServlet("/HelloServlet") 
 public class HelloServlet
-extends HttpServlet { 
+ extends HttpServlet { 
 public HelloServlet() { 
 super(); 
 } 
